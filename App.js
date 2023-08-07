1d84bc924c7639d8b6ea3fd46f4b60f791bf2fbd
Aleixopi15 <aleixop1515@gmail.com>
2023-08-06 22:18:54 -0300

    bug em tab navigation

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Routes from "./src/routes";
-
+import RoutesHome from "./src/routes";
 
 export default function App() {
   return(
     <NavigationContainer>
-      <Routes/>
+      <Routes>
+        <RoutesHome/>
+        </Routes>
+      
     
     </NavigationContainer>
+
   );
 };
