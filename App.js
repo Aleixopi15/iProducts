c54d479c7139e1ac2827a506a12aad58ba8f7c46
Aleixopi15 <aleixop1515@gmail.com>
2023-08-06 21:22:03 -0300

    test

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Routes from "./src/routes";
+import RoutesTwo from "./src/routestwo"
 
 export default function App() {
   return(
     <NavigationContainer>
       <Routes/>
+    
     </NavigationContainer>
   );
 };
