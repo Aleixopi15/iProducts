5085cc2f822d24d061beecf8f10eee90b1cdc73c
Aleixopi15 <aleixop1515@gmail.com>
2023-08-10 23:03:56 -0300

    Interface provisoria

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -6,36 +6,42 @@ import {
   TextInput,
   TouchableOpacity,
 } from "react-native";
+import { Input, Icon, Stack, Pressable, Center, NativeBaseProvider, Button, Link, Checkbox } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import * as Animation from "react-native-animatable";
 
 export default function Signin() {
   const navigation = useNavigation();
-
+  const [show, setShow] = React.useState(false);
   return (
-    <View style={styles.container}>
-      <Animation.View animation="fadeInLeft" style={styles.containerHeader}>
-        <Text>Bem-vindo(a)</Text>
-      </Animation.View>
+    <Stack space={10} w="100%" p="100" alignItems="center">
+   <Input borderRadius={'15'} w={{
+    base: "140%",
+    md: "25%"
+  }} InputLeftElement={<Icon as={<MaterialIcons name="person" />} size={5} ml="2" color="#4ECB19" />} placeholder="Nome" />
+    <Input borderRadius={'15'} w={{
+    base: "140%",
+    md: "25%"
+  }}  placeholder="Email" />
+    <Input borderRadius={'15'} w={{
+    base: "140%",
+    md: "25%"
+  }} type={show ? "text" : "password"} InputRightElement={<Pressable onPress={() => setShow(!show)}>
+          <Icon as={<MaterialIcons name={show ? "visibility" : "visibility-off"} />} size={5} mr="2" color="#4ECB19" />
+        </Pressable>} placeholder="Senha" />
+        <Input borderRadius={'15'} w={{
+    base: "140%",
+    md: "25%"
+  }} type={show ? "text" : "password"} InputRightElement={<Pressable onPress={() => setShow(!show)}>
+          <Icon as={<MaterialIcons name={show ? "visibility" : "visibility-off"} />} size={5} mr="2" color="#4ECB19" />
+        </Pressable>} placeholder="Confirmar Senha" />
+        <Checkbox>
+        Concondo com os termos de uso e privacidade
+      </Checkbox>
+        <Button  w={{base: "100%"}} borderRadius={'15'} background={'#4ECB19'} onPress={() => navigation.navigate('SignIn')}>Criar conta</Button>
+  </Stack>
 
-      <Animation.View animation="fadeInUp" style={styles.containerForm}>
-        <Text style={styles.title}>Email</Text>
-        <TextInput placeholder="Digite um e-mail..." styles={styles.input} />
-        <Text style={styles.title}>Senha</Text>
-        <TextInput placeholder="Sua senha" styles={styles.input} />
-
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Criar Conta</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={styles.buttonRegister}
-          onPress={() => navigation.navigate("SignIn")}
-        >
-          <Text style={styles.registerText}>Voltar para o Login</Text>
-        </TouchableOpacity>
-      </Animation.View>
-    </View>
   );
 }
 
